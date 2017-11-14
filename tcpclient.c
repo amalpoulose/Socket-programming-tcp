a2976e2c4940d56311c002773c9bbed7a0cd86d8
Amal Poulose <amalpoulose1995@hotmail.com>
2017-11-14 23:33:26 +0530

    Add files via upload

---
+++ tcpclient.c
@@ -0,0 +1,45 @@
+
+#include"header.h"
+
+int main(int argc ,char **argv)
+{
+
+ if(argc !=3)
+{
+ printf("Usage:./client port_number ip_address\n");
+ return;
+}
+int sfd,r,len;
+struct sockaddr_in v;
+sfd=socket(AF_INET,SOCK_STREAM,0);
+if(sfd<0)
+{
+perror("socket");
+return;
+}
+
+v.sin_family=AF_INET;
+v.sin_port=htons(atoi(argv[1]));
+v.sin_addr.s_addr=inet_addr(argv[2]);
+len=sizeof(v);
+if(connect(sfd,(struct sockaddr*)&v,len)==-1)
+{
+  perror("connect");
+  return ;
+}
+
+char s[500];
+bzero(s,sizeof(s));
+while(1)
+{
+  printf("String : ");
+  scanf(" %s",s);
+  write(sfd,s,strlen(s)+1);
+  if(strcmp(s,"exit")==0)
+   return;
+  read(sfd,s,sizeof(s));
+  printf("%s",s);
+}
+
+return 0;
+}
