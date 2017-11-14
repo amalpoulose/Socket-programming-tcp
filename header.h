ab566218a8e3ea003fa03d5b2acb7186a5b432da
Amal Poulose <amalpoulose1995@hotmail.com>
2017-11-14 23:33:21 +0530

    Add files via upload

---
+++ header.h
@@ -0,0 +1,14 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<signal.h>
+#include<unistd.h>
+
+#include<netinet/in.h>
+#include<netinet/ip.h>
+#include<arpa/inet.h>
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+	
